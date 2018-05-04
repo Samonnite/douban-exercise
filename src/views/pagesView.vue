61db132817dcd6d1c3eaad04866e51ca6c07645b
Saomonite <981003163@qq.com>
2018-05-04 15:52:56 +0800

    douban-exercise

---
+++ src/views/pagesView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pages-view">
+    <header-bar @showTalion="open"></header-bar>
+    <router-view></router-view>
+    <talion-view v-show="talion" @closeTalion="close"></talion-view>
+  </div>
+</template>
+<script>
+  import HeaderBar from '../components/headerBar'
+  import TalionView from '../views/talionView'
+  export  default {
+    name: 'pages-view',
+    components: {
+      HeaderBar,
+      TalionView
+    },
+    data () {
+      return {
+        talion: ''
+      }
+    },
+    methods: {
+      open: function () {
+        this.talion = 'open'
+      },
+      close: function () {
+        this.talion = ''
+      }
+    }
+  }
+</script>
